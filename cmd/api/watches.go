1b2219b99b07844c6d510fad693a6fe2a0fd5c0f
Aleke7 <aleke7.kz@gmail.com>
2023-10-07 22:48:09 +0600

    finished 1-4 chapters

--- cmd/api/watches.go
+++ cmd/api/watches.go
@@ -3,13 +3,50 @@ package main
 import (
 	"fmt"
 	"jewelry.abgdrv.com/internal/data"
+	"jewelry.abgdrv.com/internal/validator"
 	"net/http"
 	"time"
 )
 
 // POST "/v1/watches"
 func (app *application) createWatchHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new watch")
+
+	var input struct {
+		Brand     string  `json:"brand"`
+		Model     string  `json:"model,omitempty"`
+		DialColor string  `json:"dial_color"`
+		StrapType string  `json:"strap_type"`
+		Diameter  int64   `json:"diameter"`
+		Energy    string  `json:"energy"`
+		Gender    string  `json:"gender"`
+		Price     float64 `json:"price"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	watch := data.Watch{
+		Brand:     input.Brand,
+		Model:     input.Model,
+		DialColor: input.DialColor,
+		StrapType: input.StrapType,
+		Diameter:  input.Diameter,
+		Energy:    input.Energy,
+		Gender:    input.Gender,
+		Price:     input.Price,
+	}
+
+	v := validator.New()
+
+	if data.ValidateWatch(v, watch); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 // GET "/v1/watches/:id"
@@ -27,6 +64,9 @@ func (app *application) showWatchHandler(w http.ResponseWriter, r *http.Request)
 		Model:     "Submariner",
 		DialColor: "Black",
 		StrapType: "Stainless Steel",
+		Diameter:  40,
+		Energy:    "Mechanical",
+		Gender:    "Male",
 		Price:     9999.99,
 	}
 
--- internal/data/watches.go
+++ internal/data/watches.go
@@ -1,6 +1,10 @@
 package data
 
-import "time"
+import (
+	"jewelry.abgdrv.com/internal/validator"
+	"strings"
+	"time"
+)
 
 type Watch struct {
 	ID        int64     `json:"id"`
@@ -9,5 +13,31 @@ type Watch struct {
 	Model     string    `json:"model,omitempty"`
 	DialColor string    `json:"dial_color"`
 	StrapType string    `json:"strap_type"`
+	Diameter  int64     `json:"diameter"`
+	Energy    string    `json:"energy"`
+	Gender    string    `json:"gender"`
 	Price     float64   `json:"price"`
+}
+
+func ValidateWatch(v *validator.Validator, watch Watch) {
+	v.Check(watch.Brand != "", "brand", "must be provided")
+	v.Check(len(watch.Brand) <= 500, "brand", "must not be more than 500 bytes long")
+
+	v.Check(watch.Model != "", "model", "must be provided")
+	v.Check(len(watch.Model) <= 500, "model", "must not be more than 500 bytes long")
+
+	v.Check(watch.DialColor != "", "dial_color", "must be provided")
+	v.Check(len(watch.DialColor) <= 500, "dial_color", "must not be more than 500 bytes long")
+
+	v.Check(watch.StrapType != "", "strap_type", "must be provided")
+	v.Check(len(watch.StrapType) <= 500, "strap_type", "must not be more than 500 bytes long")
+
+	v.Check(strings.ToLower(watch.Gender) == "male" && watch.Diameter >= 38 && watch.Diameter <= 46, "diameter", "for men should be between 38mm and 46mm")
+	v.Check(strings.ToLower(watch.Gender) == "female" && watch.Diameter >= 26 && watch.Diameter <= 36, "diameter", "for women should be between 26mm and 36mm")
+
+	v.Check(strings.ToLower(watch.Energy) == "mechanical" || strings.ToLower(watch.Energy) == "quartz", "energy", "fall into two main categories: Mechanical and Quartz")
+
+	v.Check(strings.ToLower(watch.Gender) == "male" || strings.ToLower(watch.Gender) == "female", "gender", "Gender can be male or female")
+
+	v.Check(watch.Price > 0, "price", "can not be equal or less than 0")
 }
