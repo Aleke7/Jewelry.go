92a617b5001aae5b6e3ada275af53b11a4aea5e2
Aleke7 <aleke7.kz@gmail.com>
2023-10-29 15:46:56 +0600

    minor

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -45,11 +45,22 @@ func main() {
 		"development",
 		"Environment (development|staging|production)")
 
-	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("WATCH_DB_DSN"), "PostgreSQL DSN")
+	flag.StringVar(&cfg.db.dsn, "db-dsn",
+		"postgres://watch_admin:watch@localhost/watch_database?sslmode=disable",
+		"PostgreSQL DSN")
 
-	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
-	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
-	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
+	flag.IntVar(&cfg.db.maxOpenConns,
+		"db-max-open-conns",
+		25,
+		"PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns,
+		"db-max-idle-conns",
+		25,
+		"PostgreSQL max idle connections")
+	flag.StringVar(&cfg.db.maxIdleTime,
+		"db-max-idle-time",
+		"15m",
+		"PostgreSQL max connection idle time")
 
 	flag.Parse()
 
